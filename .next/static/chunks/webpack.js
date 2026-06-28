5faa95e6c8b5ef07bc82f1b06ef5ee8ef659e155
manavtdesign <manavt.design@gmail.com>
2026-06-28 14:23:30 +1000

    Updated app-build-manifest.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0b7077d0d933a729"; }
+/******/ 		__webpack_require__.h = function() { return "5040fc9d109d2fa9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
